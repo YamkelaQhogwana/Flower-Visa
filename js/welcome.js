b201eab8f768654383487bbca8ed818e7b0eb226
YamkelaQhogwana <133680348+YamkelaQhogwana@users.noreply.github.com>
2023-06-07 17:53:52 +0200

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -2,9 +2,9 @@ document.getElementById('nameForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('nameInput').value;
     var greeting = document.getElementById('greeting');
-    greeting.textContent = 'Hello ' + name + '! Welcome to FlowerVisa Prototype. Explore our cutting-edge features, including an interactive fashion gallery, search functionality, personalized recommendations, and user-friendly navigation. The default username is proto, the default user email is proto@gmail.com and the default password is proto';
+    greeting.textContent = 'Hello ' + name + '! Welcome to FlowerVisa Prototype One. Please Feel Free to click and interact.You can view this prototype from any device as it is interactive! ';
     greeting.style.display = 'block';
     document.getElementById('nameForm').style.display = 'none';
     document.getElementById('prototype-link').style.display = 'block';
-  });
+  })
   
